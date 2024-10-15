493365d9314a0791f30a5be84178710a2f749c7a
Rodrigo301239 <rodrigoifpr1@gmail.com>
2024-10-15 16:16:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int opcao,numero, resultado;
+        int aleatorio = random.nextInt(10)+1 ;
+        int calc1, calc2;
+
+        System.out.println("Escolha:\n1 - PAR\n2 - IMPAR");
+        opcao = scanner.nextInt();
+
+        if (opcao == 1){
+            System.out.println(aleatorio);
+            System.out.println("Digite seu numero");
+            numero = scanner.nextInt();
+            resultado = numero + aleatorio;
+
+            calc1 = resultado / 2;
+            calc2 = calc1 * 2;
+
+            if (calc2 == resultado){
+                System.out.println("o par ganhou");
+            }
+            else if (calc2 != resultado){
+                System.out.println("o impar ganhou");
+            }
+        }
+        else {
+            System.out.println(aleatorio);
+            System.out.println("Digite seu numero");
+            numero = scanner.nextInt();
+            resultado = numero + aleatorio;
+
+            calc1 = resultado / 2;
+            calc2 = calc1 * 2;
+
+            if (calc2 == resultado){
+                System.out.println("o par ganhou");
+            }
+            else if (calc2 != resultado){
+                System.out.println("o impar ganhou");
+            }
+        }
+
+
+        }
+
+
+    }
+
