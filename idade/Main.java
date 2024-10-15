dc425120a20bcc70f7a572cf65f8e97d9452aaf1
Rodrigo301239 <rodrigoifpr1@gmail.com>
2024-10-15 16:56:38 -0300

    calculo idade

---
+++ idade/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int idade;
+
+        System.out.println("Digite sua idade");
+        idade = scanner.nextInt();
+
+        if (idade <= 13){
+            System.out.println("deivin quer seu insta");
+        }
+        else if (idade > 13 && idade < 18){
+            System.out.println("kawan quer seu zap");
+        }
+        else if (idade > 18){
+            System.out.println("Passou do ponto pro Deyvin");
+        }
+        else {
+
+        }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
